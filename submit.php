1bde1741fadbd4cff204f69f0c7dc8f3aea9baf7
kristianklimek <klimekkristian@gmail.com>
2018-05-03 13:21:00 +0200

    submit opravený

--- submit.php
+++ submit.php
@@ -11,10 +11,10 @@ $KCGPU = 10; //index spotřebitelů
 $KCCPU = 10; //index spotřebitelů
 $KCRAM = 16; //GB
 $KCHDD = 40; //GB
-$myGPU = filter_input (INPUT_POST, 'KCGPU');
-$myCPU = filter_input (INPUT_POST, 'KCCPU');
-$myRAM = filter_input (INPUT_POST, 'KCRAM'); //GB
-$myHDD = filter_input (INPUT_POST, 'KCHDD'); //GB
+$myGPU = filter_input (INPUT_POST, 'myGPU');
+$myCPU = filter_input (INPUT_POST, 'myCPU');
+$myRAM = filter_input (INPUT_POST, 'myRAM'); //GB
+$myHDD = filter_input (INPUT_POST, 'myHDD'); //GB
 
     if (($KCGPU<=$myGPU) && ($KCCPU<=$myCPU) && ($KCRAM<=$myRAM) && ($KCHDD<=$myHDD)){
     echo "Užij si hru";
